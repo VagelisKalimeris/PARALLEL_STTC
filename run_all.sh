38790e2310ce08caf7b56a2dcd279922cdb48c00
Orestis <mousore@ics.forth.gr>
2019-06-27 16:29:18 +0300

    Clean Repository for users' safety
    Removed every Dataset

--- run_all.sh
+++ run_all.sh
@@ -4,17 +4,14 @@ clear
 
 date +"%F %H:%M:%S:%N"
 
-time ./sttc 500 9 r1r_027_3
-time ./sttc 500 9 r1r_041_3
-
-time ./sttc 500 6 r5l_033_3
-time ./sttc 500 8 r5l_034_3
+time ./sttc 500 0 scankey1_v2_full_8826cells_3-dc
 
 date +"%F %H:%M:%S:%N"
 
-time ./sttc 500 2 FS13_SA_OGB_adult_reduced_th2
-time ./sttc 500 2 PL3_SA_OGB_adult_reduced_th2
-time ./sttc 500 2 PL5_SA_OGB_adult_reduced_th2
-time ./sttc 500 2 PL12_SA_OGB_adult_reduced_th2
+time ./sttc 500 2 scankey1_v2_full_8826cells_3-dc
+
+date +"%F %H:%M:%S:%N"
+
+time ./sttc 500 4 scankey1_v2_full_8826cells_3-dc
 
 date +"%F %H:%M:%S:%N"
