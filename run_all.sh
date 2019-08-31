5089854465090b5526619de3516cd8daf1b24118
Orestis <mousore@ics.forth.gr>
2019-08-31 17:33:57 +0300

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,16 +2,16 @@ make clean
 make
 clear
 
-date +"%F %H:%M:%S:%N"
+#date +"%F %H:%M:%S:%N"
 
-time ./sttc 500 0 scankey1_v2_full_8826cells_3-dc
-
-date +"%F %H:%M:%S:%N"
-
-time ./sttc 500 2 scankey1_v2_full_8826cells_3-dc
+#time ./sttc 500 0 scankey1_v3_full_8826cells_3-dc
 
 date +"%F %H:%M:%S:%N"
 
-time ./sttc 500 4 scankey1_v2_full_8826cells_3-dc
+time ./sttc 500 2 scankey1_v3_full_8826cells_3-dc
 
 date +"%F %H:%M:%S:%N"
+
+#time ./sttc 500 4 scankey1_v3_full_8826cells_3-dc
+
+#date +"%F %H:%M:%S:%N"
